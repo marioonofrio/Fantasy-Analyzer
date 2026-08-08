2377c37f24e62f131d55374d33394b24decf2322
marioonofrio <marofrio7@gmail.com>
2026-08-08 17:22:32 -0500

    add amplify origin to cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,7 +6,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://main.xxxxxxxxx.amplifyapp.com",
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
